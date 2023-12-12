ee9e6e1626b8adfd0c8ce445dd3737f1d88cd67b
Administrator <Administrator@VMI1507335>
2023-12-12 19:12:15 +0100

    Add Commands and Events

---
+++ SM-Post/Post.Common/Events/MessageUpdatedEvent.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Post.Common.Events
+{
+    public class MessageUpdatedEvent : BaseEvent
+    {
+        public MessageUpdatedEvent() : base(nameof(MessageUpdatedEvent)){
+
+        }
+        public string Message {get; set;}
+    }
+}
